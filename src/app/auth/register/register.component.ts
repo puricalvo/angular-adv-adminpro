8dd39149e9f86b399536df7063dc0776c4708138
Puri Calvo <Puri_adrian@hotmail.com>
2023-08-07 06:21:26 +0200

    fin seccion 3 - diseño

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.Component.css']
+})
+export class RegisterComponent {
+
+}
